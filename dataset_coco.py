eb3b6a93462fe56dcdc427afd14f642408f4f881
Jakub Wojciechowski <jakub@snaprapid.com>
2018-01-02 11:36:51 +0100

    Initial commit

---
+++ dataset_coco.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  2 11:01:20 2018
+Plik przygotowujący obrazy ze zbioru COCO przy użyciu API i narzędzi Kerasa
+@author: jakub
+"""
+
