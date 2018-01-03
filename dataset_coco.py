6279f657803d3032570e79fc63a9725d3ad6556f
Jakub Wojciechowski <jakub@snaprapid.com>
2018-01-03 13:01:00 +0100

    mask generation added

--- dataset_coco.py
+++ dataset_coco.py
@@ -5,4 +5,65 @@ Created on Tue Jan  2 11:01:20 2018
 Plik przygotowujący obrazy ze zbioru COCO przy użyciu API i narzędzi Kerasa
 @author: jakub
 """
+from pycocotools.coco import COCO
+from pycocotools import mask as maskUtils
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+import os
+import cv2
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
+dataDir='/home/jakub/data/fxi/coco'
+dataType='val2017'
+annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
+# initialize COCO api for instance annotations
+coco=COCO(annFile)
+
+# get all images containing given categories, select one at random
+catIds = coco.getCatIds(catNms=['person','cat']);
+imgIds = coco.getImgIds(catIds=catIds );
+#imgIds = coco.getImgIds(imgIds = [324158])
+img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+img_path = os.path.join(dataDir, dataType, img['file_name'])
+
+I = io.imread(img_path)
+plt.axis('off')
+plt.imshow(I)
+plt.show()
+
+# load and display instance annotations
+plt.imshow(I); plt.axis('off')
+annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco.loadAnns(annIds)
+coco.showAnns(anns)
+
+# initialize COCO api for person keypoints annotations
+annFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir,dataType)
+coco_kps=COCO(annFile)
+
+plt.imshow(I); plt.axis('off')
+ax = plt.gca()
+annIds = coco_kps.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco_kps.loadAnns(annIds)
+coco_kps.showAnns(anns)
+polygons = []
+mask = np.zeros((I.shape[0], I.shape[1]), dtype = np.float32)
+
+for ann in anns:
+    if 'segmentation' in ann:
+        if type(ann['segmentation']) == list:
+            # polygon
+            for seg in ann['segmentation']:
+                poly = np.array(seg).reshape((int(len(seg)/2), 2))
+                poly = np.expand_dims(poly, 1).astype(np.int32)
+                mask = cv2.drawContours(mask, [poly], 0, 1.0,-1)
+        if 'keypoints' in ann and type(ann['keypoints']) == list:
+            # turn skeleton into zero-based index
+            sks = np.array(coco_kps.loadCats(ann['category_id'])[0]['skeleton'])-1
+            kpn = coco_kps.loadCats(ann['category_id'])[0]['keypoints']
+            kp = np.array(ann['keypoints'])
+            x = kp[0::3]
+            y = kp[1::3]
+            v = kp[2::3]
