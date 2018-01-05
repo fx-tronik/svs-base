fc626ce1dd03429cdb59f198f88fa40cfd77730a
Jakub Wojciechowski <jakub@snaprapid.com>
2018-01-05 15:29:34 +0100

    first complete version

--- dataset_coco.py
+++ dataset_coco.py
@@ -15,6 +15,7 @@ import os
 import cv2
 import random
 from multiprocessing import Process, Queue
+import string
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 #cat - category
 #img - image
@@ -22,7 +23,7 @@ pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
 class Dataset:
     def __init__(self, dataDir, imageSize, targetSize, batchSize,
-                 minLabelArea = 256., maxUpscale = 2., minUnderscale = 0.6):
+                 minLabelArea=256., maxUpscale=2., minUnderscale=0.6):
         self.dataDir = dataDir
         self.imageSize = imageSize
         self.targetSize = targetSize
@@ -33,7 +34,6 @@ class Dataset:
         self.initImages()
         self.initWorkers()
 
-        
     def initImages(self):
         #change with final dataset!
         trainType, valType = 'val2017', 'val2017'
@@ -59,26 +59,32 @@ class Dataset:
         self.cats = cats
         self.weights = weights
         self.borders = borders
-    def addSuperCat(self, ann):
+    def addSuperCat(self, anns):
         cats = self.cats
-        annSupCat = [cat['supercategory'] for cat in cats \
-                     if cat['id'] == ann['category_id']][0]
-        ann['supercategory_id'] = self.sCatNms.index(annSupCat)
-        
+        for ann in anns:
+            if not 'category_id' in ann:
+                raise ValueError("Wrong format of annotation")
+            annSupCat = [cat['supercategory'] for cat in cats \
+                         if cat['id'] == ann['category_id']][0]
+            ann['supercategory_id'] = self.sCatNms.index(annSupCat)
+
+
     def generateInstancesTargets(self, img, anns, coco):
         sCatNms = self.sCatNms
         w, h = img['width'], img['height']
-        targets = [np.zeros((h, w), dtype = np.float32) for sCat in sCatNms]
+        targets = [np.zeros((h, w), dtype=np.float32) for _ in sCatNms]
         for ann in anns:
             sCatId = ann['supercategory_id']
             targets[sCatId] += coco.annToMask(ann).astype(np.float32)
         instTargets = np.stack(targets)
-    def generateKeypointsTargets(self, img, anns, coco, tarDim=2):
+        return instTargets
+
+    def generateKeypointsTargets(self, img, anns, tarDim=2):
         #generate target from person keypoints annotation
         #targets are point, but extended circles with radius tarDim
         kpCatNms = self.kpCatNms
         w, h = img['width'], img['height']
-        targets = [np.zeros((h, w), dtype = np.float32) for kpCat in kpCatNms]
+        targets = [np.zeros((h, w), dtype=np.float32) for _ in kpCatNms]
         for ann in anns:
             if 'keypoints' in ann and type(ann['keypoints']) == list:
                 # turn skeleton into zero-based index
@@ -95,14 +101,12 @@ class Dataset:
                         vis = 1 -> not visible but labeled
                         vis = 2 -> visible and labeled
                         '''
-                        print i, xCor, yCor, tarDim, vis
-                        cv2.circle(targets[i], (xCor, yCor),tarDim, vis, -1)
+                        cv2.circle(targets[i], (xCor, yCor), tarDim, vis, -1)
         kpTargets = np.stack(targets)
-        
-            
-        
+        return kpTargets          
+
     def generateMask(self, totalTargets, borderW=5, 
-                     strictBMlp=4.0, fuzzyBMlp = 0.0):
+                     strictBMlp=4.0, fuzzyBMlp=0.0):
         '''
         strictBorder - weight on border more important than inside of instance
                        borders more precise, harder to train
@@ -112,16 +116,17 @@ class Dataset:
         borders = self.borders
         masks = np.copy(totalTargets)
         masks *= weights.reshape((-1, 1, 1))
-        kernel = np.ones((borderW, borderW),np.float32)
+        kernel = np.ones((borderW, borderW), np.float32)
         for mask, border in zip(masks, borders):
             if border:
-                dilation = cv2.dilate(mask,kernel,iterations = 1)
+                dilation = cv2.dilate(mask, kernel, iterations=1)
                 outBorder = dilation - mask
             mask[mask == 0] = 1.0
             if border == 'strict':
                 mask[outBorder > 0] = strictBMlp
             elif border == 'fuzzy':
                 mask[outBorder > 0] = fuzzyBMlp
+        return masks
         
     def scaleStride(self, img, anns):
         #generate random scale and stride to select patch from image
@@ -134,45 +139,86 @@ class Dataset:
         imageSize = self.imageSize
         scale = minUnderscale + (maxUpscale - minUnderscale)*random.random()
         w, h = img['width'], img['height']
-        minImgLabel = np.min([ann['area'] for ann in anns])
+        labelAreas = [ann['area'] for ann in anns]
+        if labelAreas:
+            minImgLabel = np.min(labelAreas)
+        else:
+            minImgLabel = 10 * minLabelArea
         #Label area should be bigger than minLabelArea - 256 (16px x 16px)
         scale = max([scale, minLabelArea / minImgLabel])
+        scale = np.min([scale, maxUpscale])
+        scale = np.max([scale] + [float(imageSize) / float(dim) for dim in [w, h]])
         wScaled, hScaled = [int(dim * scale) for dim in [w, h]]
-        stride = [np.random.randint(0, dim - imageSize) for dim in [wScaled, hScaled]]
+        stride = [np.random.randint(0, dim - imageSize+1) for dim in [wScaled, hScaled]]
         return scale, stride
 
-    def cutPatch(self, I, scale = 1.0, stride = [0, 0]):
-        # TODO!
+    def cutPatch(self, I, scale=1.0, stride=[0, 0]):
+        imageSize = self.imageSize
         if len(np.shape(I)) == 2:
-            I = np.expand_dims(I, axis = 0)
-        
+            I = np.expand_dims(I, axis=0)
+        patch = []
+        for ch in I:
+            ch = cv2.resize(ch, None, fx=scale, fy=scale)
+            ch = ch[stride[1]:stride[1]+imageSize, stride[0]:stride[0]+imageSize]
+            patch.append(ch)
+        patch = np.stack(patch)
+        return patch
     def batchGenerator(self, queue, val=False):
         cocos = self.cocos
         dataDir = self.dataDir
         types = self.types
+        addSuperCat = self.addSuperCat
+        personInAnns = self.personInAnns
+        batchSize = self.batchSize
         workingCocos = cocos[val::2] #instances and person keypoints
         dataType = types[val]
         imgIds = workingCocos[0].getImgIds()
         seed = 7
         random.seed(seed)
         random.shuffle(imgIds)
-        imgIter = 0        
+        imgIter = 0  
+        def popContainer(container, batchSize):
+            data = container[:batchSize]
+            container = container[batchSize:]
+            print 'I POOP', len(container)
+            return np.stack(data, axis = 0)
+        iCont, tCont, mCont = [], [], []
         while(1):
             imgId = imgIds[imgIter]
             img = workingCocos[0].loadImgs(imgId)[0] #only one image
             annIds = workingCocos[0].getAnnIds(imgIds=img['id'])
             anns = workingCocos[0].loadAnns(annIds)
-            for ann in anns:
-                self.addSuperCat(ann)
+            addSuperCat(anns)
+                
             imgPath = os.path.join(dataDir, dataType, img['file_name'])
-            I = cv2.imread(imgPath, 0)
-            cv2.namedWindow('test', cv2.WINDOW_NORMAL)
-            cv2.imshow('test', I)
-            cv2.waitKey(1000)
-            cv2.destroyWindow('test')
+            image = cv2.imread(imgPath, 0)
+            image = image.astype(np.float32) / 255.0
+            
+            iTargets = self.generateInstancesTargets(img, anns, workingCocos[0])
+            kpAnns = []
+            if personInAnns(anns):
+                kpAnnIds = workingCocos[1].getAnnIds(imgIds=img['id'])
+                kpAnns = workingCocos[1].loadAnns(kpAnnIds)
+            kpTargets = self.generateKeypointsTargets(img, kpAnns)
+            targets = np.append(iTargets, kpTargets, axis = 0)
+            masks = self.generateMask(targets)
+            
+            scale, stride = self.scaleStride(img, anns)
+            [image, targets, masks] = map(lambda t: self.cutPatch(t, scale, stride), 
+                                          [image, targets, masks])
+            iCont.append(image)
+            tCont.append(targets)
+            mCont.append(masks)
             imgIter+=1
-            queue.put((1,2,3))
-            print '123 added', val
+            print 'LEN', len(iCont), batchSize
+            while (len(iCont) >= batchSize):
+                iB = popContainer(iCont, batchSize)
+                tB = popContainer(tCont, batchSize)
+                mB = popContainer(mCont, batchSize)
+                queue.put((iB, tB, mB))
+            
+
+
     def initWorkers(self):
         self.trainQ = None
         self.valQ = None
@@ -182,73 +228,46 @@ class Dataset:
         self.trainP.start()
         self.valP = Process(target=self.batchGenerator, args=(self.valQ, True))
         self.valP.start()
+    def endDataset(self):
+        self.trainP.terminate()
+        self.valP.terminate()
+        self.trainQ = None
+        self.valQ = None
+    def personInAnns(self, anns):
+        #check if there is person in annotations -> then use keypoints anns
+        for ann in anns:
+            if ann['category_id'] == 1:
+                return True
+        return False #if no person in annotations
+    def showTensor(self, t):
+        if len(t.shape) == 3:
+            tShow = np.sum(t, axis=0)
+        elif len(t.shape) == 2:
+            tShow = np.copy(t)
+        else:
+            print 'Invalid tensor format'
+            return False
+        chrs = list(string.ascii_lowercase)
+        random.shuffle(chrs)
+        wId = ''.join(chrs[:5])
+        winName = 'test_{}'.format(wId)
+        cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+        cv2.imshow(winName, tShow)
+        cv2.waitKey(0)
+        cv2.destroyWindow(winName)
+    def iterateMinibatches(self, val = False):
+        valBatches = 8
+        queue = self.valQ if val else self.trainQ
+        batches = valBatches if val else 20 * valBatches
+        for batchId in range(batches):
+            yield queue.get()
         
 if __name__ == "__main__":
     dataDir = '/home/jakub/data/fxi/coco'
     dataset  = Dataset(dataDir=dataDir, imageSize=256, targetSize=40, 
-                       batchSize=32)
+                       batchSize=8)
+    for inputs, targets, masks in dataset.iterateMinibatches(val=False):
+        dataset.showTensor(inputs[0])
+    dataset.endDataset()
 
-
-    dataType = 'val2017'
-    annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
-    # initialize COCO api for instance annotations
-    coco = COCO(annFile)
-    # display COCO categories and supercategories
-    cats = coco.loadCats(coco.getCatIds())
-    nms = [cat['name'] for cat in cats]
-    print 'COCO categories: \n{}\n'.format(' '.join(nms))
-    
-    nms = set([cat['supercategory'] for cat in cats])
-    print 'COCO supercategories: \n{}'.format(' '.join(nms))
-    # get all images containing given categories, select one at random
-    catIds = coco.getCatIds(catNms=['person','cat'])
-    #catIds = coco.getCatIds()
-    imgIds = coco.getImgIds(catIds=catIds )
-    #imgIds = coco.getImgIds(imgIds = [324158])
-    img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-    img_path = os.path.join(dataDir, dataType, img['file_name'])
-    
-    I = io.imread(img_path)
-    plt.axis('off')
-    plt.imshow(I)
-    plt.show()
-    
-    # load and display instance annotations
-    plt.imshow(I); plt.axis('off')
-    annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-    anns = coco.loadAnns(annIds)
-    for ann in anns:
-        dataset.addSuperCat(ann)
-    coco.showAnns(anns)
-    
-    # initialize COCO api for person keypoints annotations
-    annFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir,dataType)
-    coco_kps=COCO(annFile)
-    
-    plt.imshow(I); plt.axis('off')
-    ax = plt.gca()
-    annIds = coco_kps.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-    anns = coco_kps.loadAnns(annIds)
-    for ann in anns:
-        dataset.addSuperCat(ann)
-    coco_kps.showAnns(anns)
-    polygons = []
-    mask = np.zeros((I.shape[0], I.shape[1]), dtype = np.float32)
-    
-    for ann in anns:
-        if 'segmentation' in ann:
-            if type(ann['segmentation']) == list:
-                # polygon
-                for seg in ann['segmentation']:
-                    poly = np.array(seg).reshape((int(len(seg)/2), 2))
-                    poly = np.expand_dims(poly, 1).astype(np.int32)
-                    mask = cv2.drawContours(mask, [poly], 0, 1.0,-1)
-            if 'keypoints' in ann and type(ann['keypoints']) == list:
-                # turn skeleton into zero-based index
-                sks = np.array(coco_kps.loadCats(ann['category_id'])[0]['skeleton'])-1
-                kpNms = coco_kps.loadCats(ann['category_id'])[0]['keypoints']
-                kps = np.array(ann['keypoints'])
-                x = kps[0::3]
-                y = kps[1::3]
-                v = kps[2::3]
-                
+#TODO BUG - > repeating images
